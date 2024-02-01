fb8c3c40cd19312314d693411ad37659bfce0a7f
test <test@test.com>
2024-02-01 17:32:02 +0900

    초기환경세팅

---
+++ src/main/java/com/ebook/EbookApplication.java
@@ -0,0 +1,15 @@
+package com.ebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class EbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbookApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/ebook/test/TestController.java
@@ -0,0 +1,46 @@
+package com.ebook.test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.ebook.mapper.LikeMapper;
+
+@Controller
+public class TestController {
+	
+	@Autowired
+	private LikeMapper likeMapper;
+	
+	@GetMapping("/test1")
+	@ResponseBody
+	public String test1() {
+		return "Hello world!"; //html
+	}
+	
+	@GetMapping("/test2")
+	@ResponseBody
+	public Map<String, Object> test2() {
+		Map<String, Object> result = new HashMap<>();
+		result.put("테스트1", 111);
+		result.put("test2", 222);
+		result.put("테스트3", 333);
+		return result; //json
+	}
+	
+	@GetMapping("/test3")
+	public String test3() {
+		return "test/test"; //3 html
+	}
+	
+	@GetMapping("test4")
+	@ResponseBody
+	public List<Map<String, Object>> test4() {
+		return likeMapper.selectLikeList(); //4 jsp
+	}
+}
